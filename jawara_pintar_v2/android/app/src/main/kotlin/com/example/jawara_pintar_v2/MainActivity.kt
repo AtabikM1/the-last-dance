6d4ee55003aad8620202ef290be7c2bdf5b3e413
RAFI ODY PRASETYO <53549077+rafiody16@users.noreply.github.com>
2025-11-11 08:22:20 +0700

    Delete jawara_pintar_v2 directory

--- jawara_pintar_v2/android/app/src/main/kotlin/com/example/jawara_pintar_v2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.jawara_pintar_v2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- jawara_pintar_v2/lib/data/login_data.dart
+++
@@ -1,8 +0,0 @@
-class LoginData {
-  static const String staticEmail = "admin@jawara.com";
-  static const String staticPassword = "123456";
-  
-  static bool validateCredentials(String email, String password) {
-    return email == staticEmail && password == staticPassword;
-  }
-}
